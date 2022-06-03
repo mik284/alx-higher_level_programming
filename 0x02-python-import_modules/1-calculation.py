44b91eb23b05f417e259f552c2099f7d171840ef
Michael Kamau <65639270+mik284@users.noreply.github.com>
2022-06-03 05:08:06 +0300

    calculator_1 import modules

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,3 +1,15 @@
 #!/usr/bin/python3
 
-def __import__ =' __main__'
+if __name__ == "__main__":
+    """
+    MIKES addition function
+    """
+    from calculator_1 import add, sub, mul, div
+
+    a = 10
+    b = 5
+
+    print("{} + {} = {}".format(a, b, add(a, b)))
+    print("{} - {} = {}".format(a, b, sub(a, b)))
+    print("{} * {} = {}".format(a, b, mul(a, b)))
+    print("{} / {} = {}".format(a, b, div(a, b)))
