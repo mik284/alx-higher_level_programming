7666fc7fa29559baed02696501279ed1ca887ade
mik284 <mikeykamau222@gmail.com>
2022-09-05 22:41:17 +0300

    made execuatable files

--- 0x12-javascript_warm_up/5-to_integer.js
+++ 0x12-javascript_warm_up/5-to_integer.js
@@ -1,5 +1,4 @@
 #!/usr/bin/node
-//Write a script that prints My number: <first argument converted in integer> if the first argument can be converted to an integer:
 function myNumber(arg1) {
     if (!parseInt(arg1)) {
         console.log("Not a Number")
