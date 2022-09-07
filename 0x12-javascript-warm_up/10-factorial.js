463b0ba8eacde51df4794e20d5fb7efe1720b569
Michael Kamau <65639270+mik284@users.noreply.github.com>
2022-09-08 00:17:30 +0300

    Update 10-factorial.js

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,12 +1,14 @@
 #!/usr/bin/node
-function factorial (n) {
-  if (n < 0) {
-    return (-1);
+/*
+  print a factorial
+*/
+
+function factorial (a) {
+  if (a === 1 || isNaN(a)) {
+    return 1;
   }
-  if (n === 0 || isNaN(n)) {
-    return (1);
-  }
-  return (n * factorial(n - 1));
+  return a * factorial(a - 1);
 }
 
-console.log(factorial(Number(process.argv[2])));
+const args = process.argv;
+console.log(factorial(parseInt(args[2])));
