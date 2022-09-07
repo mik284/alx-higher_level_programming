c848a869f2d46fdeac1cd662ba2e87d2b28ed3e1
Michael Kamau <65639270+mik284@users.noreply.github.com>
2022-09-08 00:22:04 +0300

    Create 12-object.js

---
+++ 0x12-javascript-warm_up/12-object.js
@@ -0,0 +1,10 @@
+cutable File  8 lines (8 sloc)  135 Bytes
+
+#!/usr/bin/node
+const myObject = {
+  type: 'object',
+  value: 12
+};
+console.log(myObject);
+myObject.value = 89;
+console.log(myObject);
