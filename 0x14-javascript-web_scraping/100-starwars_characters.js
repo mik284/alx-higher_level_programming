c6485d2b4af37a1c4a79cbfa87fc8f64c94815e1
Michael Kamau <65639270+mik284@users.noreply.github.com>
2022-12-09 12:44:20 +0300

    Update 100-starwars_characters.js

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -1,17 +1,18 @@
 #!/usr/bin/node
+// A script that prints all characters of a Star Wars movie
 
 const request = require('request');
-
-request('http://swapi.co/api/films/' + process.argv[2], function (error, response, body) {
-  if (error) {
-    console.error(error);
-  }
-  JSON.parse(body).characters.forEach(function (url, callback) {
-    request(url, function (error, response, body) {
-      if (error) {
-        console.error(error);
-      }
-      console.log(JSON.parse(body).name);
+const film = process.argv[2];
+const url = `https://swapi-api.hbtn.io/api/films/${film}`;
+request(url, (err, res, body) => {
+  if (!err) {
+    const characters = JSON.parse(body).characters;
+    characters.forEach((character) => {
+      request(character, function (err, res, body) {
+        if (!err) {
+          console.log(JSON.parse(body).name);
+        }
+      });
     });
-  });
+  }
 });
