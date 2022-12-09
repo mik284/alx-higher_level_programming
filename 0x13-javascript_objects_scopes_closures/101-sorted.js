f23f2c4381c5146cb27bb1e8669fe5142baf911b
Michael Kamau <65639270+mik284@users.noreply.github.com>
2022-12-09 11:55:27 +0300

    Update 101-sorted.js

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,9 +1,13 @@
 #!/usr/bin/node
+// Script imports a dictionary of occurrences by user id
+// and computes a dictionary of user ids by occurrence.
 
-const { dict } = require('./101-data');
-
-const myValue = Object.entries(dict).reduce((acc, [key, value]) => {
-  acc[value] = acc[value] ? [...acc[value], key] : [key];
-  return acc;
-}, {});
-console.log(myValue);
+const { dict } = require('./101-data.js');
+const Dictn = {};
+for (const N in dict) {
+    if (Dictn[dict[N]] === undefined) {
+	Dictn[dict[N]] = [];
+    }
+    Dictn[dict[N]].push(N);
+}
+console.log(Dictn);
